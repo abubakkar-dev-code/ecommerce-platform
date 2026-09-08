fbbbb77092cc7a623fe0b4932dd34079dd693bb0
abubakkar291 <sulthan.yoho@gmail.com>
2026-09-08 15:05:34 +0530

    added the routes for create,get,update and remve and clear for cart controlelrs

--- backend/src/controllers/cart.controller.ts
+++ backend/src/controllers/cart.controller.ts
@@ -73,3 +73,132 @@ export const createCart = async (
     next(error);
   }
 };
+export const getCart = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  try {
+    const userId = req.user?.userId;
+    if (!userId) {
+      throw new ApiError(400, "please login to see your cart");
+    }
+    const cart = await Cart.findOne({ user: userId })
+      .populate("items.product")
+      .populate("items.varient");
+    if (!cart) {
+      throw new ApiError(400, "Cart is Empty,keep shopping");
+    }
+    const cartItems = cart.items.map((item) => {
+      const itemTotal = item.price * item.quantity;
+
+      return {
+        ...item.toObject(),
+        itemTotal,
+      };
+    });
+    const totalAmount = cartItems.reduce(
+      (total, item) => total + item.itemTotal,
+      0,
+    );
+    res.status(200).json(
+      new ApiResponse("Cart fetched successfully", {
+        cartItems,
+        totalAmount,
+      }),
+    );
+  } catch (error) {
+    next(error);
+  }
+};
+export const updateCart = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  try {
+    const userId = req.user?.userId;
+    if (!userId) {
+      throw new ApiError(401, "Please login to update your cart");
+    }
+    const { varientId } = req.params;
+    const { quantity } = req.body;
+    if (quantity < 1) {
+      throw new ApiError(400, "quantity must be at least 1");
+    }
+    const cart = await Cart.findOne({ user: userId });
+    if (!cart) {
+      throw new ApiError(400, "Cart is Empty");
+    }
+    const requiredItem = cart.items.find(
+      (item) => item.varient.toString() === varientId,
+    );
+    if (!requiredItem) {
+      throw new ApiError(404, "Item not found");
+    }
+    const inventory = await Inventory.findOne({ varient: varientId });
+    if (!inventory) {
+      throw new ApiError(404, "Inventory not found");
+    }
+    if (inventory.quantity < quantity) {
+      throw new ApiError(
+        400,
+        `Stocks are limited,we have only ${inventory.quantity} items left`,
+      );
+    }
+    requiredItem.quantity = quantity;
+    await cart.save();
+    res.status(200).json(new ApiResponse("Cart updated successfully", cart));
+  } catch (error) {
+    next(error);
+  }
+};
+export const removeCart = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  try {
+    const userId = req.user?.userId;
+    if (!userId) {
+      throw new ApiError(404, "unauthorized,please login");
+    }
+    const { varientId } = req.params;
+    const cart = await Cart.findOne({ user: userId });
+    if (!cart) {
+      throw new ApiError(400, "cart is empty");
+    }
+    const existingVarient = cart.items.find(
+      (item) => item.varient.toString() === varientId,
+    );
+    if (!existingVarient) {
+      throw new ApiError(404, "item not found in cart");
+    }
+    cart.items.pull({ varient: varientId });
+    await cart?.save();
+    res.status(200).json(new ApiResponse("Item removed from cart", cart));
+  } catch (error) {
+    next(error);
+  }
+};
+export const clearCart = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  try {
+    const userId = req.user?.userId;
+    if (!userId) {
+      throw new ApiError(400, "unauthorized,please login");
+    }
+    const cart = await Cart.findOne({ user: userId });
+    if (!cart) {
+      throw new ApiError(400, "cart is empty");
+    }
+    cart.items.splice(0, cart.items.length);
+    await cart.save();
+    res.status(200).json(new ApiResponse("Cart cleared successfully", cart));
+  } catch (error) {
+    next(error);
+  }
+};
